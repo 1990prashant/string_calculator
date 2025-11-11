827face422426942c3fb7c49df9a1971b4872715
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 23:18:44 +0530

    Added a failing case for negative number in the input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -17,5 +17,41 @@ describe StringCalculator do
         expect { calculator.add(['123']) }.to raise_error('Only accept string as an input')
       end
     end
+
+    context 'without any delimeter' do
+      it 'should return same number' do
+        expect(calculator.add('12')).to eq(12)
+      end
+    end
+
+    context 'with comma delimeter' do
+      it 'should return sum of numbers' do
+        expect(calculator.add('3, 5, 3')).to eq(11)
+      end
+    end
+
+    context 'with new line delimeter' do
+      it 'should return sum of numbers' do
+        expect(calculator.add('3\n5\n3')).to eq(11)
+      end
+    end
+
+    context 'with multiple delimeter' do
+      it 'should return sum of numbers' do
+        expect(calculator.add('3;\n5;\n3')).to eq(11)
+      end
+    end
+
+    context 'with predefined delimiters' do
+      it 'should return sum of numbers' do
+        expect(calculator.add('//;\n3;5;3')).to eq(11)
+      end
+    end
+
+    context 'with negative values' do
+      it 'should raise error with negative numbers' do
+        expect { calculator.add('3;-5;3;-9') }.to raise_error('Negative numbers not allowed <-5, -9>')
+      end
+    end
   end
 end
