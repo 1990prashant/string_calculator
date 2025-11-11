18fc149989776d6315d5de70aa33741addb8f491
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 23:04:23 +0530

    Added add method to make the test pass for empty string

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,2 +1,6 @@
 class StringCalculator
+  def add(input)
+    numbers = input.split(/[^0-9-]+/).map(&:to_i)
+    numbers.sum
+  end
 end
--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,6 +1,9 @@
 require './lib/string_calculator'
 
 describe StringCalculator do
+
+  subject(:calculator) { described_class.new }
+
   describe 'add' do
     context 'given an empty string' do
       it 'should return 0' do
