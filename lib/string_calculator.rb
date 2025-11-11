c072edec3bb5c152917ab1f051f80e5bd5296ae7
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 23:27:00 +0530

    Refactored add method to validate and raise exception for negative numbers

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -2,10 +2,16 @@ class StringCalculator
   def add(input)
     validate_input(input)
     numbers = input.split(/[^0-9-]+/).map(&:to_i)
+    validate_for_negative_numbers(numbers)
     numbers.sum
   end
 
   private
+
+  def validate_for_negative_numbers(numbers)
+    negative_numbers = numbers.select(&:negative?)
+    raise "Negative numbers not allowed <#{negative_numbers.join(', ')}>" unless negative_numbers.empty?
+  end
 
   def validate_input(input)
     raise 'Only accept string as an input' unless input.is_a? String
