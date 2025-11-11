06bb8ff1f939a0e1229b491b80e37f5fe79df266
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 23:11:16 +0530

    Added a failing test for invalid input

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -10,5 +10,12 @@ describe StringCalculator do
         expect(calculator.add('')).to eq(0)
       end
     end
+
+    context 'given input other than string type' do
+      it 'should raise error' do
+        expect { calculator.add(123) }.to raise_error('Only accept string as an input')
+        expect { calculator.add(['123']) }.to raise_error('Only accept string as an input')
+      end
+    end
   end
 end
