89adcfdc2ced5e5042b4dcd3d5c158e62ce891e7
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 23:12:55 +0530

    Added ability to validate input to pass the tests

--- lib/string_calculator.rb
+++ lib/string_calculator.rb
@@ -1,6 +1,13 @@
 class StringCalculator
   def add(input)
+    validate_input(input)
     numbers = input.split(/[^0-9-]+/).map(&:to_i)
     numbers.sum
+  end
+
+  private
+
+  def validate_input(input)
+    raise 'Only accept string as an input' unless input.is_a? String
   end
 end
