5705e1ba44fa167a3c68ac4d096e231f14e136f1
Punchh_PrashantM <prashant.mishra@punchh.com>
2025-11-11 22:59:07 +0530

    Added a failing test case for add method with empty string

--- spec/string_calculator_spec.rb
+++ spec/string_calculator_spec.rb
@@ -1,4 +1,11 @@
 require './lib/string_calculator'
 
 describe StringCalculator do
+  describe 'add' do
+    context 'given an empty string' do
+      it 'should return 0' do
+        expect(calculator.add('')).to eq(0)
+      end
+    end
+  end
 end
